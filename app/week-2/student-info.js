2bdbc1e4015c3c587501df3259bb9674ab56d7cd
denistorjai <denistorjai@outlook.com>
2024-09-06 14:24:15 -0600

    Finished week 2 assignment

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,10 +1,10 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function StudentInfo() {
     return (
         <div> 
             <h1> Denis Torjai </h1>
-            <Link href="https://github.com/denistorjai/cprg306-assignments"> My github repository </Link>
+            <Link href="https://github.com/denistorjai/cprg306-assignments"> Click me for github repository Link </Link>
         </div>
     )
 }
