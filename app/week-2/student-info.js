d3af9930fe23509259f64aade4614f5ab4ab53fc
denistorjai <denistorjai@outlook.com>
2024-09-06 14:18:58 -0600

    Page and Student Info

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function StudentInfo() {
+    return (
+        <div> 
+            <h1> Denis Torjai </h1>
+            <Link href="https://github.com/denistorjai/cprg306-assignments"> My github repository </Link>
+        </div>
+    )
+}
