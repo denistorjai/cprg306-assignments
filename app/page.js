c8b8e498df40aa826581df92fe164ec3ad5f101d
denistorjai <denistorjai@outlook.com>
2024-09-06 16:18:27 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,9 +5,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main> 
-      <h1> CPRG 306: Web Development 2 - Assignment </h1>
-      <Link href="./week-2"> Week 2 Assignment </Link>
-      <Link href="./week-3"> Week 3 Assignment </Link>
+      <h1 className="text-4xl font-bold mb-5" > CPRG 306: Web Development 2 - Assignment </h1>
+      <ul> 
+        <li> <Link href="./week-2" className="hover:text-green-400 hover:underline"> Week 2 Assignment </Link> </li> 
+        <li> <Link href="./week-3" className="hover:text-green-400 hover:underline"> Week 3 Assignment </Link> </li> 
+      </ul>
     </main>
   );
 }
