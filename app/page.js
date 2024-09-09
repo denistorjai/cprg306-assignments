629029ce087e8f0d33917a0b05a9f1cc3275769b
denistorjai <denistorjai@outlook.com>
2024-09-09 09:06:21 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,7 +8,8 @@ export default function Home() {
       <h1 className="text-4xl font-bold mb-5" > CPRG 306: Web Development 2 - Assignment </h1>
       <ul> 
         <li> <Link href="./week-2" className="hover:text-green-400 hover:underline"> Week 2 Assignment </Link> </li> 
-        <li> <Link href="./week-3" className="hover:text-green-400 hover:underline"> Week 3 Assignment </Link> </li> 
+        <li> <Link href="./week-3" className="hover:text-green-400 hover:underline"> Week 3 Assignment </Link> </li>
+         
       </ul>
     </main>
   );
