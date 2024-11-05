90a4afc6f7d19ff418f9007adba710a12d91e6ce
denistorjai <denistorjai@outlook.com>
2024-11-05 13:41:15 -0700

    Week 8

--- app/page.js
+++ app/page.js
@@ -13,6 +13,7 @@ export default function Home() {
         <li> <Link href="./week-5" className="hover:text-purple-800 hover:underline"> Week 5 Assignment </Link> </li>
         <li> <Link href="./week-6" className="hover:text-purple-800 hover:underline"> Week 6 Assignment </Link> </li>
         <li> <Link href="./week-7" className="hover:text-purple-800 hover:underline"> Week 7 Assignment </Link> </li>
+        <li> <Link href="./week-8" className="hover:text-purple-800 hover:underline"> Week 8 Assignment </Link> </li>
       </ul>
     </main>
   );
